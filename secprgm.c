8bb69696daa3d8583a039fd5f0dfbc5a7b718e64
indixpert990512 <vinod990512@gmail.com>
2025-12-16 20:00:17 +0530

    no chnge

---
+++ secprgm.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   printf("my name is Vinod Kumar Sharma: ");
+   printf("\ni am Post Graduate: ");
+   printf("\nfrom Jharkhand: ");
+   printf("\ni am Learning Full stack Devlopment: ");
+
+
+    return 0;
+}
